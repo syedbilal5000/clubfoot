65cf86edbc91bd4b8210e022a65a8841c63ab247
Syed Bilal Hussain <syedbilalhussain168@gmail.com>
2021-12-28 17:09:25 +0500

    Made the changes for the first demo

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Route::get('/', function () {
     return redirect('login');
 });
 
-Auth::routes(['register' => false]);
+Auth::routes();
+// Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
