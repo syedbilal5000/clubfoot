3a658276a9b9072d9b0de27a58ef6959c7a37b55
Bilal Saeed <bilal.saeed@tpsonline.com>
2022-01-02 15:29:39 +0500

    checkin

--- resources/views/appointment/index.blade.php
+++ resources/views/appointment/index.blade.php
@@ -1,93 +1,46 @@
+@extends('layouts.admin')
 
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Calendar</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="http://localhost:81/AdminLTE-2.4.5/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://localhost:81/AdminLTE-2.4.5/bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="http://localhost:81/AdminLTE-2.4.5/bower_components/Ionicons/css/ionicons.min.css">
-  <!-- fullCalendar -->
-  <link rel="stylesheet" href="http://localhost:81/AdminLTE-2.4.5/bower_components/fullcalendar/dist/fullcalendar.min.css">
-  <link rel="stylesheet" href="http://localhost:81/AdminLTE-2.4.5/bower_components/fullcalendar/dist/fullcalendar.print.min.css" media="print">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="http://localhost:81/AdminLTE-2.4.5/dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="http://localhost:81/AdminLTE-2.4.5/dist/css/skins/_all-skins.min.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Google Font -->
-  <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
-<body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
-  <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <!-- /.col -->
-        <div class="col-md-9">
-          <div class="box box-primary">
-            <div class="box-body no-padding">
-              <!-- THE CALENDAR -->
-              <div id="calendar"></div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /. box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
+@section('content')
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Appointment</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="home">Home</a></li>
+            <li class="breadcrumb-item active">Appointment Page</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
-  <div class="control-sidebar-bg"></div>
-</div>
-<!-- ./wrapper -->
+  <!-- /.content-header -->
 
-<!-- jQuery 3 -->
-<script src="http://localhost:81/AdminLTE-2.4.5/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="http://localhost:81/AdminLTE-2.4.5/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="http://localhost:81/AdminLTE-2.4.5/bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Slimscroll -->
-<script src="http://localhost:81/AdminLTE-2.4.5/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="http://localhost:81/AdminLTE-2.4.5/bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="http://localhost:81/AdminLTE-2.4.5/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="http://localhost:81/AdminLTE-2.4.5/dist/js/demo.js"></script>
-<!-- fullCalendar -->
-<script src="http://localhost:81/AdminLTE-2.4.5/bower_components/moment/moment.js"></script>
-<script src="http://localhost:81/AdminLTE-2.4.5/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
-<!-- Page specific script -->
+{{-- Main Content --}}
+
+<form method="POST" action="appointment/add">
+  @csrf
+  <div class="row">
+    <!-- /.col -->
+    <div class="col-md-9">
+      <div class="box box-primary">
+        <div class="box-body no-padding">
+          <!-- THE CALENDAR -->
+          <div id="calendar"></div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /. box -->
+    </div>
+    <!-- /.col -->
+  </div>
+</form>
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/moment/moment.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/fullcalendar/dist/fullcalendar.min.js') }}"></script>
 <script>
   $(function () {
 
@@ -249,5 +202,4 @@
     })
   })
 </script>
-</body>
-</html>
+@endsection
