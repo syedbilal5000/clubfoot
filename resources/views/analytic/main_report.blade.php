234392aeb730bdba3e54d9f5cb6d3d81a77f9a38
Hussain <bilal.hussain@afiniti.com>
2022-06-27 16:58:35 +0500

    Edit Main Report

--- resources/views/analytic/main_report.blade.php
+++ resources/views/analytic/main_report.blade.php
@@ -25,13 +25,13 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0">Appointments Delayed</h1>
+          <h1 class="m-0">Main Report</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="../home">Home</a></li>
             <li class="breadcrumb-item"><a href="./">Analytics</a></li>
-            <li class="breadcrumb-item active">Appointments Delayed</li>
+            <li class="breadcrumb-item active">Main Report</li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
